6b155a0ab83592b89be9c0bcab90512a9d4d9180
Prashani <aruniprashani@gmail.com>
2021-10-12 10:53:10 +0530

    Implemented division method

--- src/main/java/it/sliit/MathOperation.java
+++ src/main/java/it/sliit/MathOperation.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 
 public class MathOperation {
 
-    public static double add(double... operands)
-    {
-        return Arrays.stream(operands).reduce(0d, Double::sum);
-    }
+
 
     public static double subtract(double operand1, double operand2)
     {
         return operand1 - operand2;
+    }
+
+    public static int div(int a, int b){
+        return a/b;
     }
 
 }
